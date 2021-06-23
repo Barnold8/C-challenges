36a88f5b20f19bdf0f213f9d8d2d38f0e613b4c4
Brandon Wright <55092542+Barnold8@users.noreply.github.com>
2021-06-23 12:17:42 +0100

    Create ForEachLoopChallenge.cpp

---
+++ ForEachLoopChallenge.cpp
@@ -0,0 +1,46 @@
+#include "pch.h"
+#include <vector>
+#include <iostream>
+#include "Node.h"
+#include "MyHeader.h"
+#include "constants.h"
+#include <string>
+#include <iterator>
+#include <cstddef>
+#include <algorithm>
+
+//Declare a fixed array with the following names
+// : Alex, Betty, Caroline, Dave, Emily, Fred, Greg, and Holly. 
+// Ask the user to enter a name.
+// Use a for each loop to see if the name the user entered is in the array.
+//Sample output :
+//Enter a name : Betty
+//Betty was found.
+//Enter a name : Megatron
+//Megatron was not found.
+
+int main()
+{
+	bool found{ false };
+	std::string names[8]{ "Alex", "Betty", "Caroline", "Dave", "Emily", "Fred", "Greg", "Holly" };
+	std::string name{};
+	std::cout << "Please enter a name: \t";
+	std::cin >> name;
+	std::cin.ignore();// cin stuff to clear cin buffer ect
+	std::cin.clear();
+
+	for (auto n : names) {
+	
+		if (name == n) {
+		
+			found = true;
+		}
+	}
+	
+	if (found) {
+		std::cout << name << " was found!";
+	}
+	else {
+		std::cout << name << " was not found!";
+	}
+}
