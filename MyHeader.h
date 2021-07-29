e9c6f8c19b55694af28df24a825babc70fcef236
Brandon Wright <55092542+Barnold8@users.noreply.github.com>
2021-07-29 12:21:03 +0100

    Update MyHeader.h

--- MyHeader.h
+++ MyHeader.h
@@ -2,8 +2,8 @@
 #include <vector>
 #include <cstdint>
 
-int add(int x, int y);
 
+//Functions ------------------------------------------------------
 void printINT(int var);
 
 template <class Any>
@@ -14,7 +14,6 @@ std::vector<int> fib(int start1, int start2, int len);
 int readNumber();
 
 void writeAnswer(int dat);
-
 
 int BinToDec(std::string data);
 
@@ -27,21 +26,50 @@ int SciNotation(int number, int exponent);
 double SciNotation(double number, double exponent);
 
 
-struct Node {
-
-	int value{};
-	Node* next;
-
-
-};
 
 //void printNodes(Node* n);
 
 
-int sumTo(int value);
+//int sumTo(int value);
 
 char* returnAlphabet();
 
+void printCharArray(char* c);
 
+std::string getString();
+
+void HowManySpaces(std::string s);
 
 static void PrintLol();
+
+// ------------------------------------------------------
+
+
+//Classes ------------------------------------------------------
+
+class Date {
+
+private:
+
+	using type = int;
+
+	type m_day{};
+	type m_month{};
+	type m_year{};
+
+public:
+
+	//Forward declaration of class functions and constructors
+
+	Date(const int&, const int&, const int&);
+
+	void setDate(const int&, const int&, const int&);
+
+	void printDate();
+
+	int getDay() { return m_day; }
+	int getMonth() { return m_month; }
+	int getYear() { return m_year; }
+
+};
+// ------------------------------------------------------
